5716d81f073e4bb53429664e3a4af4e4268f3e5b
tnyenvy <tnyenvy.2003@gmail.com>
2024-11-27 15:05:49 +0700

    Update requirements.txt

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,7 +1,33 @@
-try:
-    import osmnx
-    import geopandas
-    import shapely
-    print("All libraries imported successfully!")
-except Exception as e:
-    print(f"Error: {e}")
+#Bài 1: Tô màu bản đồ nước Úc
+from simpleai.search import CspProblem, backtrack
+
+def constraint_func(names, values):
+    return values[0] != values[1]
+
+# Tự tô màu cho miền tây
+if __name__=='__main__':
+    names = ('WA', 'NT', 'Q', 'NSW', 'V','SA','T')
+    domain = {'WA':  ['G'],
+              'NT':  ['R', 'G', 'B'],
+              'Q':   ['R', 'G', 'B'],
+              'NSW': ['R', 'G', 'B'],
+              'V':   ['R', 'G', 'B'],
+              'SA':  ['R', 'G', 'B'],
+              'T':   ['R', 'G', 'B'],
+              }
+
+    constraints = [
+        (('SA', 'WA'), constraint_func),
+        (('SA', 'NT'), constraint_func),
+        (('SA', 'Q'), constraint_func),
+        (('SA', 'NSW'), constraint_func),
+        (('SA', 'V'), constraint_func),
+        (('WA', 'NT'), constraint_func),
+        (('NT', 'Q'), constraint_func),
+        (('Q', 'NSW'), constraint_func),
+        (('NSW', 'V'), constraint_func),
+    ]
+
+    problem = CspProblem(names, domain, constraints)
+    output = backtrack(problem)
+    print(output)
